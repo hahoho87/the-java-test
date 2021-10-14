74e9bb4c3f60f239b15fa1e908e78453b35d4e4e
bky4564 <bky4564@hunet.co.kr>
2021-10-15 08:29:27 +0900

    JUnit 반복 테스트 적용

--- src/test/java/com/hahoho87/thejavatest/StudyTest.java
+++ src/test/java/com/hahoho87/thejavatest/StudyTest.java
@@ -1,27 +1,15 @@
 package com.hahoho87.thejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StudyTest {
 
-    @Test
-    @Tag("fast")
-    void createTest() {
-        Study study = new Study();
-        System.out.println("create1");
-        assertThat(study).isNotNull();
-    }
-
-    @Test
-    @Tag("slow")
-    void createTest2() {
-        System.out.println("create2");
-    }
-
     @BeforeAll
-   static void beforeAll() {
+    static void beforeAll() {
         System.out.println("Before All");
     }
 
@@ -41,5 +29,30 @@ class StudyTest {
         System.out.println("Before Each");
     }
 
+    @Test
+    @Tag("fast")
+    void createTest() {
+        Study study = new Study();
+        System.out.println("create1");
+        assertThat(study).isNotNull();
+    }
 
+    @Test
+    @Tag("slow")
+    void createTest2() {
+        System.out.println("create2");
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    @DisplayName("반복 테스트")
+    void repeatedTest(RepetitionInfo info) {
+        System.out.println(info.getCurrentRepetition() + "/" + info.getTotalRepetitions());
+    }
+
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @DisplayName("파라미터 변경 반복 테스트")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message) {
+        System.out.println("message = " + message);
+    }
 }
