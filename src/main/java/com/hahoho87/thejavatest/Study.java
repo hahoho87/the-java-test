c693df58f2cc79917f0204546ceb003b7f2ed4b4
bky4564 <bky4564@hunet.co.kr>
2021-10-15 09:13:04 +0900

    parameterized 테스트 다양한 argument 적용

--- src/main/java/com/hahoho87/thejavatest/Study.java
+++ src/main/java/com/hahoho87/thejavatest/Study.java
@@ -1,4 +1,36 @@
 package com.hahoho87.thejavatest;
 
 public class Study {
+
+    private int limit;
+
+
+    private String name;
+
+    public int getLimit() {
+        return limit;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Study() {
+    }
+
+    public Study(int limit) {
+        this.limit = limit;
+    }
 }
--- src/test/java/com/hahoho87/thejavatest/StudyTest.java
+++ src/test/java/com/hahoho87/thejavatest/StudyTest.java
@@ -1,7 +1,16 @@
 package com.hahoho87.thejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -54,5 +63,50 @@ class StudyTest {
     @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
     void parameterizedTest(String message) {
         System.out.println("message = " + message);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Parameterized 테스트")
+    @ValueSource(ints = {10, 20, 30})
+    void parameterizedTestWithIntegers(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println("study = " + study.getLimit());
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertThat(Study.class).isEqualTo(targetType);
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    @DisplayName("CsvSource 테스트")
+    @ParameterizedTest
+    @CsvSource({"10, '자바 스터디", "20, 스프링"})
+    void csvSourceTest(int limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println("study = " + study);
+    }
+
+    @DisplayName("ArgumentsAccessor 테스트")
+    @ParameterizedTest
+    @CsvSource({"10, '자바 스터디", "20, 스프링"})
+    void argumentsAccessorTest(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println("study = " + study);
+    }
+
+    @DisplayName("ArgumentAccessor 테스트 with custom aggregator")
+    @ParameterizedTest
+    @CsvSource({"10, '자바 스터디", "20, 스프링"})
+    void argumentsAccessorTestWithCustomAggregator(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println("study = " + study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
     }
 }
