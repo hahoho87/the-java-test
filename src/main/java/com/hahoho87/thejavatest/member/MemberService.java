5220ade249407a3d15b89ce1991a6c127cff017f
bky4564 <bky4564@hunet.co.kr>
2021-11-08 13:11:49 +0900

    refactoring find member logic with optional

--- src/main/java/com/hahoho87/thejavatest/member/MemberService.java
+++ src/main/java/com/hahoho87/thejavatest/member/MemberService.java
@@ -3,6 +3,8 @@ package com.hahoho87.thejavatest.member;
 import com.hahoho87.thejavatest.domain.Member;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MemberService {
 
@@ -12,8 +14,7 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
-    public Member findById(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("Member doesn't exist for id : " + memberId));
+    public Optional<Member> findById(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
--- src/main/java/com/hahoho87/thejavatest/study/StudyService.java
+++ src/main/java/com/hahoho87/thejavatest/study/StudyService.java
@@ -4,6 +4,8 @@ import com.hahoho87.thejavatest.domain.Member;
 import com.hahoho87.thejavatest.domain.Study;
 import com.hahoho87.thejavatest.member.MemberService;
 
+import java.util.Optional;
+
 public class StudyService {
 
     private final StudyRepository studyRepository;
@@ -15,8 +17,8 @@ public class StudyService {
     }
 
     public Study createStudy(Long memberId, Study study) {
-        Member member = memberService.findById(memberId);
-        study.setOwner(member);
+        Optional<Member> member = memberService.findById(memberId);
+        study.setOwner(member.orElseThrow(() -> new IllegalArgumentException("Member doesn't exist for id : " + memberId)));
         return studyRepository.save(study);
     }
 }
